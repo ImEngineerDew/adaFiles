63ecd9843da8af19cec500d845388606cea5f74e
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-18 17:10:02 -0500

    New Ada code that use a for cycle

--- forloops.adb
+++ forloops.adb
@@ -4,9 +4,6 @@ use  Ada.Text_IO;
 procedure forloops is
 	number:Integer;
 begin
-	Put_Line("Escriba un número a recorrrer: ");
-	number:= Integer'value(Ada.Text_IO.Get_Line);
-
 	for number in 0..9 loop
 		Put_Line(Integer'Image(number));
 	end loop;
