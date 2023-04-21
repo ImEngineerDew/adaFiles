4b53065c000180b807a99582af9764491bbfadc0
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-21 17:22:06 -0500

    Finished code

--- number003/imengineerdew.adb
+++ number003/imengineerdew.adb
@@ -2,31 +2,66 @@ with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 
 procedure imengineerdew is
-   Size : Integer;
-   type ArrayIntegers is array (Positive range <>) of Integer;
-   vectorA : ArrayIntegers;
-   vectorB : ArrayIntegers;
-
-   procedure readVector (A, B : ArrayIntegers ; i : Integer);
+   type ArrayIntegers is array (Integer range <>) of Integer;
    
-   procedure readVector (A, B : ArrayIntegers ; i : Integer) is
+   --A quickly function that gets a size of your array
+   function getVectorSize return integer is 
+       Size : Integer;
    begin
-        Put_Line("This is a read vector");
-   end readVector;
+       Get(Size);
+       return size;
+   end getVectorSize;
 
-   procedure showVector (A, B : ArrayIntegers ; i : Integer);
+   procedure readVector (A, B : in out ArrayIntegers ; i : in Integer);
+   procedure showVector (A, B : in ArrayIntegers ; i : in Integer);
+   procedure resultSum  (A, B : in ArrayIntegers  ; i : in Integer);
    
-   procedure showVector (A, B : ArrayIntegers ; i : Integer) is
+   procedure readVector (A, B : in out ArrayIntegers ; i : in Integer) is
    begin
-       Put_Line("This is a show vector");
-   end showVector;
- 
+        if i = A'Length or i = B'Length then
+            return;
+        end if;
+        if i >= A'First and i <= A'Last and i >= B'First and i <= B'Last then
+           Get (A(i));
+           Get (B(i));     
+           readVector (A, B , i+1);
+        end if;
+   end readVector;   
+   
+   procedure showVector (A, B : in ArrayIntegers ; i : in Integer) is
+   begin
+       if i = A'Length or i = B'Length then
+           return;
+       end if;
+       -- Check if the values of A and B are inside or outside
+       if i >= A'First and i <= A'Last and i >= B'First and i <= B'Last then
+           Put (A(i));
+           Put (B(i));     
+           showVector (A, B , i+1);
+        end if;
+   end showVector;   
+   
+   procedure resultSum (A, B : in ArrayIntegers ; i : in Integer) is
+   begin
+       if i = A'Length or i = B'Length then
+           return;
+       end if;
+       -- Check if the values of A and B are inside or outside
+       if i >= A'First and i <= A'Last and i >= B'First and i <= B'Last then
+           Put (A(i) + B(i));
+           resultSum (A, B , i+1); 
+       end if;
+   end resultSum;   
+
+--- This is the public static void main(String args[]) {} of Ada
 begin
-   Get(Size);
-   vectorA := ArrayIntegers (1 .. size);
-   vectorB := ArrayIntegers (1 .. size);
-
-   -- put here the recursion
-   readVector (vectorA , vectorB , 0);
-   showVector (vectorA , vectorB , 0);
+   declare
+      vectorSize : Integer := getVectorSize;
+      --The position of ArrayIntegers is zero
+      vectorA , vectorB : ArrayIntegers (0 .. vectorSize); 
+   begin
+      -- put here the recursion
+      readVector (vectorA , vectorB , 1);
+      resultSum (vectorA , vectorB , 1);
+   end;
 end imengineerdew;
